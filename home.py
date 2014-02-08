4707257618063c79edf6036b6d1a2434b0179c9a
zyxstar <248340019@qq.com>
2014-02-08 12:50:46 +0800

    edit

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ app_root = os.path.dirname(__file__)
 
 handlers.RES_BASE_URL_PATH = "/static"
 handlers.SAMPLE_NOTE_URL = "https://raw2.github.com/zyxstar/md_note/master/docs/Manual.md"
-handlers.render = web.template.render(os.path.join(app_root, '/static/templates'))
+handlers.render = web.template.render(os.path.join(app_root, 'static/templates'))
 
 app = web.application(handlers.urls, handlers.all_handlers).wsgifunc()
 
